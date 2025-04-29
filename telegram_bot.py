dfe68b653b0685aa9b15923bc76218161775d5ff
lyuda140707 <lyuda14070702@icloud.com>
2025-04-29 12:25:36 +0300

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.utils import executor
+import os
+
+API_TOKEN = os.getenv("BOT_TOKEN")  # або просто встав токен напряму як рядок
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="📱 Відкрити меню", web_app=WebAppInfo(url="https://lyuda140707.github.io/telegram-recipe-webapp/"))]
+    ])
+    await message.answer("Привіт! 👋 Щоб почати — натисни кнопку нижче 👇", reply_markup=keyboard)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
