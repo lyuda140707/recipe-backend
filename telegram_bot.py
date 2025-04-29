dbe0825464611375a25957376125456fdec60a1e
lyuda140707 <lyuda14070702@icloud.com>
2025-04-29 12:55:30 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-from aiogram.utils.executor import start_polling
+from aiogram.utils import executor  # ✅ правильний імпорт для версії 2.25.1
 import os
 
 API_TOKEN = os.getenv("BOT_TOKEN")  # має бути додано в Render → Environment
