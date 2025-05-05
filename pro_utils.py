522f316fa12bd9f776fbda1d94c74e122aa5ea54
lyuda140707 <lyuda14070702@icloud.com>
2025-05-05 10:57:15 +0300

    Create pro_utils.py

---
+++ pro_utils.py
@@ -0,0 +1,21 @@
+import gspread
+import json
+import os
+from datetime import datetime
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Завантаження credentials
+with open('/etc/secrets/credentials.json', 'r') as f:
+    credentials_info = json.load(f)
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+])
+client = gspread.authorize(credentials)
+spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1zJOrLr_uNCL0_F7Qcrgwg_K0YCSEy9ISoWX_ZUDuSYg/edit")
+pro_worksheet = spreadsheet.worksheet("pro_users")
+
+def add_pro_user(user_id: int, username: str, name: str):
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    pro_worksheet.append_row([str(user_id), username or "", name, now])
